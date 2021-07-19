8ce79f048bb871833cac5d9ceb379356df958259
Elijahscriptdev <elijahobominuru@gmail.com>
2021-07-19 22:47:23 +0100

    created reusable component

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import CardItem from "./components/cardItem";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.carContainer}>
-        <ImageBackground
-          source={require("./assets/images/ModelX.jpeg")}
-          style={styles.image}
-        />
-        <View style={styles.titles}>
-          <Text style={styles.title}>Model S</Text>
-          <Text style={styles.subTitle}>Starting at $86,000</Text>
-        </View>
-      </View>
+      <CardItem />
       <StatusBar style='auto' />
     </View>
   );
@@ -27,27 +19,5 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  carContainer: {
-    width: "100%",
-    height: "100%",
-  },
-  titles: {
-    width: "100%",
-    marginTop: "30%",
-    alignItems: "center",
-  },
-  title: {
-    fontSize: 40,
-    fontWeight: "600",
-  },
-  subTitle: {
-    fontSize: 16,
-    color: "#5c5e62",
-  },
-  image: {
-    width: "100%",
-    height: "100%",
-    resizeMode: "cover",
-    position: "absolute"
-  },
+  
 });
